551e4e7156491358f7489060fa6f1bb0aa0382a1
wangkaimin <wangkaimin@zcgames.cn>
2017-08-21 18:09:11 +0800

    tm not use

--- model/models.go
+++ model/models.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 
 	"blog.ka1em.site/common"
-	"github.com/gorilla/schema"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var DB *gorm.DB
-var SchemaDecoder = schema.NewDecoder()
 
 const (
 	DBHost = "47.93.11.105"
