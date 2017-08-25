1df515f1baa872c933376ea1e2aaf0edad7b0d29
wangkaimin <wangkaimin@zcgames.cn>
2017-08-25 14:40:39 +0800

    update

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
-	"blog.ka1em.site/common"
+	"time"
+
 	"blog.ka1em.site/router"
+	"github.com/urfave/negroni"
 )
 
 const PORT = ":8443"
@@ -12,10 +15,22 @@ const PORT = ":8443"
 func main() {
 	r := router.InitRouters()
 
+	n := negroni.Classic() // 导入一些预设的中间件
+	n.UseHandler(r)
+
+	s := &http.Server{
+		Addr:           PORT,
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
+
 	//http.ListenAndServeTLS(PORT, "keys/blog.ka1em.site/214098123750645.pem",
 	//	"keys/blog.ka1em.site/214098123750645.key", r)
 
-	common.Suggar.Debug("Listening...")
-
-	http.ListenAndServe(PORT, r)
+	//common.Suggar.Debug("Listening...")
+	//
+	//http.ListenAndServe(PORT, r)
 }
