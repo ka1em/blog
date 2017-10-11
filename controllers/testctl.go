7e59fba9b9424433b6d49030357da0472467fe8b
ka1em <wangkaiminchn@gmail.com>
2017-10-11 20:11:31 +0800

    test

--- controllers/testctl.go
+++ controllers/testctl.go
@@ -9,6 +9,6 @@ import (
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	zlog.ZapLog.Info("%+v", r.Form)
+	zlog.ZapLog.Info("%+v %s %s", r.Form, r.RemoteAddr, r.RequestURI)
 	return
 }
