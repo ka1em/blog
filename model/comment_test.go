626c9de34179d76bddbd1ea4a8b1270861b4a29a
ka1em <wangkaiminchn@gmail.com>
2017-12-01 21:04:14 +0800

    rm unuse test

--- model/comment_test.go
+++ model/comment_test.go
@@ -2,11 +2,7 @@ package model
 
 import (
 	"testing"
-
-	"github.com/jinzhu/gorm"
 )
-
-var tdb *gorm.DB
 
 //func TestMain(m *testing.M) {
 //	//dbConn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s",
@@ -19,23 +15,5 @@ var tdb *gorm.DB
 //	//os.Exit(m.Run())
 //}
 func TestCommentAddCommentTrascation(t *testing.T) {
-	tx := tdb.Begin()
 
-	c1 := Comment{ID: 4}
-	if err := tx.Create(&c1).Error; err != nil {
-		tx.Rollback()
-		t.Fatal(err.Error())
-	}
-
-	c2 := Comment{ID: 4}
-	if err := tx.Create(&c2).Error; err != nil {
-		tx.Rollback()
-		t.Fatal(err.Error())
-	}
-
-	err := tx.Commit().Error
-	if err != nil {
-		t.Fatalf("test trascation error")
-		return
-	}
 }
