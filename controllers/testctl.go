f5288852788c03b059cc8fb39748e64159d104dd
ka1em <wangkaiminchn@gmail.com>
2017-12-04 22:36:28 +0800

    test

--- controllers/testctl.go
+++ controllers/testctl.go
@@ -7,13 +7,16 @@ import (
 	"blog/model"
 )
 
+var Count = 0
+
 // TestHandler 测试handler
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 	data := model.GetBaseData()
 	if err := r.ParseForm(); err != nil {
 		zlog.ZapLog.Error(err.Error())
 	}
-
+	Count++
+	data.Data["count"] = Count
 	zlog.ZapLog.Info("%+v %s %s", r.Form, r.RemoteAddr, r.RequestURI)
 	data.Data["remoteAddr"] = r.RemoteAddr
 	data.Data["requestURI"] = r.RequestURI
