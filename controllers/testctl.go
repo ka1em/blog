598fac137a5bd0d0d8fab8327fccb80d51338055
ka1em <wangkaiminchn@gmail.com>
2017-12-04 23:12:33 +0800

    ad rwmutex

--- controllers/testctl.go
+++ controllers/testctl.go
@@ -2,12 +2,22 @@ package controllers
 
 import (
 	"net/http"
+	"sync"
 
 	"blog/common/zlog"
 	"blog/model"
 )
 
-var Count = 0
+type Counter struct {
+	Count int
+	m     *sync.RWMutex
+}
+
+var Count Counter
+
+func init() {
+	Count.m = new(sync.RWMutex)
+}
 
 // TestHandler 测试handler
 func TestHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,8 +25,15 @@ func TestHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		zlog.ZapLog.Error(err.Error())
 	}
-	Count++
+
+	Count.m.Lock()
+	Count.Count++
+	Count.m.Unlock()
+
+	Count.m.RLock()
 	data.Data["count"] = Count
+	Count.m.RUnlock()
+
 	zlog.ZapLog.Info("%+v %s %s", r.Form, r.RemoteAddr, r.RequestURI)
 	data.Data["remoteAddr"] = r.RemoteAddr
 	data.Data["requestURI"] = r.RequestURI
